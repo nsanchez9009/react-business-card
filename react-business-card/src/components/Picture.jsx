b997ec0ad11f1e7f20a748c1767572ea3360b6ae
Nicolas Sanchez <nsanchez9009@gmail.com>
2023-04-25 16:07:15 -0400

    Remaking with vite because for some reason nothing was rendering.

--- react-business-card/src/components/Picture.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Picture(){
-    return (
-        <div>Hello world</div>
-        //<img src="../assets/thefrog.jpg" className="picture"></img>
-    )
-}
