ba1f45ab75036c9b3c469985f314242647a169af
Nicolas Sanchez <nsanchez9009@gmail.com>
2023-04-25 16:05:18 -0400

    Init

--- react-business-card/src/App.jsx
+++ react-business-card/src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import React from "react"
 
-function App() {
+import './App.css'
+import Picture from './components/Picture.jsx'
+import Name from "./components/Name.jsx"
+import Contact from "./components/Contact.jsx"
+import About from "./components/About.jsx"
+import Projects from "./components/Projects.jsx"
+
+export default function App() {
   return(
-    
+    <div>
+      Hello world
+    </div>
   )
 }
-
-export default App
---
+++ react-business-card/src/components/Picture.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Picture(){
+    return (
+        <div>Hello world</div>
+        //<img src="../assets/thefrog.jpg" className="picture"></img>
+    )
+}
